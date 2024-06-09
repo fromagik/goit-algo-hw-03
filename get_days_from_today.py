82f6c333b89480d6fff93b3c5a5c063b78db8305
Slavik <yaroslav.salko71@gmail.com>
2024-06-09 17:32:02 +0200

    Rewrote function return

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -6,11 +6,10 @@ def get_days_from_today(date): #Створюємо функцію
             user_date = datetime.strptime(date, '%Y-%m-%d') #Переводимо отриману дату в фотмат дати 
             currant_date = datetime.today() # Отримуємо теперішьню дату
             difference_date = currant_date - user_date # Розраховуємо різницю
-            print(f"Різниця між вказаною датою {user_date.date()} та сьогодні {currant_date.date()} складає {difference_date.days} днів" )
-            break # Завершення циклу
+            return f"Різниця між вказаною датою {user_date.date()} та сьогодні {currant_date.date()} складає {difference_date.days} днів" # Завершення циклу
         except ValueError: # Обробка винятку
             print("Невірно введена дата. Спробуйте ще раз, та пам'ятайте про формат РРРР-ММ-ДД")
             date = input('Введіть дату в форматі РРРР-ММ-ДД: ')
         
 date = input('Введіть дату в форматі РРРР-ММ-ДД: ') 
-get_days_from_today(date) # Виклик функції 
+print(get_days_from_today(date)) # Виклик функції 
