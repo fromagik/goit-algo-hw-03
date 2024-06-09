0a7ba431f41790f0063630457ec91e3aa82d2996
Slavik <yaroslav.salko71@gmail.com>
2024-06-09 21:07:58 +0200

    Fixed a bug with the "quantity" value exceeding the maximum value.

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -2,8 +2,8 @@ import random #Імпортуємо модуль
 
 def get_numbers_ticket(min, max, quantity): #Створення функції
     lottery_numbers = [] #створення списку
-    if min < 1 or max > 1000: #Перевірка обовязковизкових умов 
-        print('Invalid parametres')
+    if min < 1 or max > 1000 or quantity > max: #Перевірка обовязковизкових умов 
+        return f'Invalid parametres'
 
     while quantity != 0: # Цикл поки довжина списку не буде рівна заданому параметру кількості
         random_num = random.randint(min, max) # Генерація випадкових чисел
@@ -12,4 +12,6 @@ def get_numbers_ticket(min, max, quantity): #Створення функції
             quantity -= 1
     
     return sorted(lottery_numbers) # Повертаємо відсортований список лотарейних чисел
+
+
 
